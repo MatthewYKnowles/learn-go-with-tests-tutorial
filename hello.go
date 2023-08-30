47dd869246ccd6631989aed8d6e772b8a76a2119
Matthew Knowles <matthewyknowles@gmail.com>
2023-08-30 12:13:33 -0400

    passed spanish test

--- hello.go
+++ hello.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
